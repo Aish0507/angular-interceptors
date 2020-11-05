c206243e7e0c88cb28007c06afd1b18e6108caf2
GersonUmanzor <gbumanzor@outlook.com>
2020-11-04 23:39:42 -0600

    :zap: Improve performance by caching get requests.

--- src/app/main/interceptors/cache.interceptor.ts
+++ src/app/main/interceptors/cache.interceptor.ts
@@ -3,16 +3,47 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { CacheService } from '../services/cache.service';
+import { tap } from 'rxjs/operators';
+import { CachedResponse } from '../interfaces/cached-response.interface';
 
 @Injectable()
 export class CacheInterceptor implements HttpInterceptor {
+  constructor(private cacheService: CacheService) {}
 
-  constructor() {}
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    if (this.cacheableRequest(request.method)) {
+      const cachedResponse = this.cacheService.get(request.url);
+      return cachedResponse
+        ? of(cachedResponse)
+        : this.submitAndCacheRequest(request, next);
+    }
+  }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  cacheableRequest(method: string): boolean {
+    // TODO: Add whitelist (if you want to cache only certain get requests).
+    return method === 'GET';
+  }
+
+  submitAndCacheRequest(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    console.log('caching');
+    return next.handle(request).pipe(
+      tap((event) => {
+        const cachedResponse: CachedResponse = {
+          response: event,
+          date: new Date(),
+        };
+        this.cacheService.set(request.url, cachedResponse);
+      })
+    );
   }
 }
