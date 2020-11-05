d0ab42c0619a48b6901b50996ee51b58cfe60631
GersonUmanzor <gbumanzor@outlook.com>
2020-11-05 10:11:39 -0600

    :sparkles: Finish error handler interceptor.

--- src/app/main/interceptors/error-handler.interceptor.ts
+++ src/app/main/interceptors/error-handler.interceptor.ts
@@ -3,16 +3,34 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorHandlerInterceptor implements HttpInterceptor {
+  constructor(private toastrService: ToastrService) {}
 
-  constructor() {}
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.displayError(error);
+        return throwError(error);
+      })
+    );
+  }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  displayError(error: HttpErrorResponse): void {
+    const { status } = error;
+    // TODO: Check different statuses to filter which of them should be displayed.
+    if (status !== 401) {
+      this.toastrService.error(error.message, 'An error occurred');
+    }
   }
 }
